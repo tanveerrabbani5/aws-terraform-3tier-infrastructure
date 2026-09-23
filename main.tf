8ed463f213cedda6149f77580fc7cf3cf9436c1f
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-23 18:08:37 +0530

    added module, vpc and tf-files -f and modified code

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr    = "10.0.0.0/16"
+  environment = var.environment
+}
+
 output "aws_account_id" {
   description = "AWS account ID being used by Terraform"
   value       = data.aws_caller_identity.current.account_id
@@ -10,4 +17,10 @@ output "aws_account_id" {
 output "aws_region" {
   description = "AWS region being used by Terraform"
   value       = data.aws_region.current.region
+}
+
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
