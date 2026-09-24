1f81ca2630339a670b50cb949ec87f1c3e045e76
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-24 19:43:25 +0530

    written code to create the private-route-table-Application and assosiate private-APP subnets in it

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -99,3 +99,21 @@ resource "aws_route_table_association" "private_db" {
   subnet_id      = aws_subnet.private_db[count.index].id
   route_table_id = aws_route_table.private_db.id
 }
+
+
+resource "aws_route_table" "private_app" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "three-tier-${var.environment}-private-app-rt"
+    Tier = "Private-App"
+  }
+}
+
+
+resource "aws_route_table_association" "private_app" {
+  count = length(var.private_app_subnet_cidrs)
+
+  subnet_id      = aws_subnet.private_app[count.index].id
+  route_table_id = aws_route_table.private_app.id
+}
