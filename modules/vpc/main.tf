ff44f10ec71ad13308d2765ce79d8c6f278cf451
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-24 18:17:36 +0530

    written code to create the public route table--> connect with igw and assosiate public subnets in it

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -57,3 +57,27 @@ resource "aws_internet_gateway" "this" {
     Name = "three-tier-${var.environment}-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "three-tier-${var.environment}-public-rt"
+    Tier = "Public"
+  }
+}
+
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
