9d21421bbcd631ac89ca03bbf9f7498b4df8cfb8
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-25 16:23:32 +0530

    written code to create the SSM permission to the IAM role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -35,3 +35,13 @@ resource "aws_iam_instance_profile" "ec2" {
     Name = "three-tier-${var.environment}-ec2-profile"
   }
 }
+
+
+##    SSM permission to the IAM role
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
