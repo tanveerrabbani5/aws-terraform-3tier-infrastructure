8d0c0ca02809aaf1d781410a20bef6b17c3d6f89
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-26 18:06:37 +0530

    written code  to create ALB Listener

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -17,6 +17,22 @@ resource "aws_lb" "this" {
 }
 
 
+##    create ALB Listener
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+
+
+
 ##    create Target Group
 
 resource "aws_lb_target_group" "app" {
