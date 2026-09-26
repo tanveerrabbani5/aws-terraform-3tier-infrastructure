41c7077634e096ae916334dad698486b6033e5d5
tanveerrabbani5 <tnvrrabbani@gmail.com>
2026-09-26 17:26:28 +0530

    written code  Scripting code to Check the web page by adding example file index.html and installing nginx

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -14,6 +14,39 @@ resource "aws_instance" "app" {
 
   associate_public_ip_address = false
 
+
+
+
+  ##    Scripting code to Check the web page by adding example file index.html and installing nginx
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -eux
+
+    apt-get update
+    apt-get install -y nginx
+
+    cat > /var/www/html/index.html <<'HTML'
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>AWS 3-Tier Infrastructure</title>
+      </head>
+      <body>
+        <h1>Deployment Successful!</h1>
+        <p>Served by a private EC2 instance.</p>
+        <p>Infrastructure provisioned using Terraform.</p>
+      </body>
+    </html>
+    HTML
+
+    systemctl enable nginx
+    systemctl start nginx
+  EOF
+
+
+
+
   tags = {
     Name        = "three-tier-${var.environment}-app"
     Tier        = "Private-App"
